2a1de285c00c696a4f9c03242e2802627a41b528
Mateus Urtassum <mateusurtassum@gmail.com>
2025-04-12 18:53:41 -0300

    First commit

---
+++ node/moduleA.js
@@ -0,0 +1,4 @@
+// Three ways to export
+this.hi = 'Hello!!!'
+exports.welcome = 'Welcome to Node!'
+module.exports.seeYou = 'See you. Bye'
---
+++ node/moduleClient.js
@@ -0,0 +1,10 @@
+// way to import
+const moduleA = require('./moduleA')
+const moduleB = require('./moduleB')
+
+console.log(moduleA.hi)
+console.log(moduleA.welcome)
+console.log(moduleA.seeYou)
+
+console.log(moduleB.goodMorning)
+console.log(moduleB.goodNight())
